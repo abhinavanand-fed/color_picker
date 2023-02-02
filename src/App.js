762f85dcac1a44d5b849430d5ade584c084f3e62
abhinavanand-fed <abhinavanand080@gmail.com>
2023-02-02 19:52:48 +0530

    colorpicker.css .js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./ColorPicker.css";
 
-function App() {
+const ColorPicker = () => {
+  const [color, setColor] = useState("#000000");
+
+  const handleChange = (event) => {
+    setColor(event.target.value);
+  };
+
+  const rgb = (hex) => {
+    var r = parseInt(hex.substring(0, 2), 16);
+    var g = parseInt(hex.substring(2, 4), 16);
+    var b = parseInt(hex.substring(4, 6), 16);
+
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="color-picker">
+      <div className="color-picker-input">
+        <input type="color" value={color} onChange={handleChange} />
+        <p>{color}</p>
+      </div>
+      <p className="color-picker-value">RGB: {rgb(color.substring(1))}</p>
+      <p className="color-picker-value">HEX: {color}</p>
     </div>
   );
-}
+};
 
-export default App;
+export default ColorPicker;
